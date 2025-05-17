51a5fc736789ffaacb88b8dac7fb9d37f8ceb38c
jrin0 <sudogrit0@gmail.com>
2025-05-17 19:43:28 +0700

    fix port

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ fastify.get('/msg', async (request, reply) => {
 fastify.get('/hsh', async (request, reply) => {
 	return getHSH();
 });
-fastify.listen({ process.env.PORT || 3000 }, () => {
+fastify.listen({ port: process.env.PORT || 3000 }, () => {
 	console.log('Server running at http://localhost:3000');
 });
 
